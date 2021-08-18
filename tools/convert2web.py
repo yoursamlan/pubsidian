f44578d370c8dc5fa284adf4291d0cec948c33cb
yoursamlan <skamlan99@gmail.com>
2021-08-18 16:31:01 +0530

    version 1.2 beta

--- tools/convert2web.py
+++ tools/convert2web.py
@@ -1,17 +1,23 @@
 
 #convert2web - bulk converter for pubsidian
-#ver 1.1-beta
+#ver 1.2-beta
 #author: Amlan Saha Kundu, 2021
 #notes: This converter is currently under-development. If you encounter with any bugs, please post it in the issue.
 #       Stable release will be coming soon.
+#fixes: Non-conversion of ![[Pasted Image]] is fixed.
+#       Non-conversion of markdown-table is fixed
 #==============================================================================================================================
 
-import markdown, re,os
+import markdown,re,os,pyimgur
 #from markdown.treeprocessors import Treeprocessor
 from markdown import markdown
 from markdown.extensions.wikilinks import WikiLinkExtension
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
+
+CLIENT_ID = "your imgur client ID, something like: 123a1bc1234567d"
+base_attachment = 'Your Obsidian Attachment Folder Path (Something like: C:/Users/blah/blah/, Please Use "/" instead of "\" or "\\". Add "/" while closing the path.)'
+
 
 def links(findex):
     linktext = ""
@@ -52,7 +58,17 @@ def createJSON(findex):
 def createIndex(uname):
     path = "./OUTPUT/index.html"
     with open(path, mode='wb') as f:
-        f.write(("<html><head><meta name='viewport' content='width=device-width, initial-scale=1'><link rel='icon' type='image/png' href='https://github.com/yoursamlan/pubsidian/blob/main/CDN/favicon.png?raw=true' /><title>pubsidian</title><link href='https://fonts.googleapis.com/css?family=Comfortaa' rel='stylesheet'><link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'><a href='https://github.com/yoursamlan' target='_blank' pubsidian id='init'></a><link rel='stylesheet' href='https://rawcdn.githack.com/yoursamlan/pubsidian/d380fd11ac87a9d58b325f1fa85892d0cdaacfc2/CDN/style.css' type='text/css'><script type="text/x-mathjax-config">MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}});</script></head><body><div id='wrapper'><div id='mySidebar' class='sidebar'><a href='javascript:void()' class='closebtn' onclick='closeNav()'>×</a><div id='content'><div id='sideheader'><h4 id='headtext'>MENU </h4></div><input type='text' id='mySearch' onkeyup='myFunction()' placeholder='Search..' title='Type in a category'><ul id='myMenu'></ul><div class='debugdiv'><h4>Dark mode</h4><div id='dbgbtn'><label class='switch'><input type='checkbox' id='dbg'><span class='slider' onclick='togdarkmode()'></span></label></div></div></div></div><div id='pagecontent'><div id='heading'><button class='openbtn' id='hamb' onclick='openNav()'>☰</button><div id='title'><h5>"+uname+"'s notebook"+"</h5></div><button class='openbtn' id='graphbtn' onclick='openGraph();setTimeout(showGraph(), 400);'>&#128376;</button></div><div id='main'><h1 id='ContentHeading'>Welcome to Pubsidian<hr></h1><div id='mainContent'><img src='https://raw.githubusercontent.com/yoursamlan/pubsidian/main/CDN/pubsidian.png' alt='logo' style='height:10%; width:auto; display:block; margin-right:auto;float:none !important;'><p>Version: 3.2.0-beta <br>Developed by: <a href='https://github.com/yoursamlan' target='_blank' style='color: red'>Amlan Saha Kundu</a></p><h3>Help</h3><ul><li>Click ☰ on top left to open the MENU.</li><li>Click on your desired note to open.</li><li>To search a note, enter the name of the note in the search-bar</li><li>Click &#128376; on top right to open the Graph.</li><li>On 'Menu', toggle the switch to enable/ disable dark mode.</li></ul></div></div></div><div id='plotgraph'><a href='javascript:void()' id='closegraphbtn' onclick='closegraph(); setTimeout(location.reload.bind(location), 200);'>×</a><div id='GraphView'></div></div></div><img src='https://leancoding.co/DOYB8A' style='width:1px !important;'><script src='https://d3js.org/d3.v3.min.js'></script><script src='https://rawcdn.githack.com/yoursamlan/pubsidian/a7da9546987b84dfc80c52178f802e212d0a6ea8/CDN/network.js' type='text/javascript'></script><script src='https://rawcdn.githack.com/yoursamlan/pubsidian/a7da9546987b84dfc80c52178f802e212d0a6ea8/CDN/script.js' type='text/javascript'></script><script src='https://rawcdn.githack.com/yoursamlan/pubsidian/a7da9546987b84dfc80c52178f802e212d0a6ea8/CDN/data.js' type='text/javascript'></script><script src='https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'></script></body></html>").encode('utf-8'))
+        f.write(("<html><head><meta name='viewport' content='width=device-width, initial-scale=1'><link rel='icon' type='image/png' href='https://github.com/yoursamlan/pubsidian/blob/main/CDN/favicon.png?raw=true' /><title>pubsidian</title><link href='https://fonts.googleapis.com/css?family=Comfortaa' rel='stylesheet'><link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'><a href='https://github.com/yoursamlan' target='_blank' pubsidian id='init'></a><link rel='stylesheet' href='https://rawcdn.githack.com/yoursamlan/pubsidian/d380fd11ac87a9d58b325f1fa85892d0cdaacfc2/CDN/style.css' type='text/css'><script type='text/x-mathjax-config'>MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}});</script></head><body><div id='wrapper'><div id='mySidebar' class='sidebar'><a href='javascript:void()' class='closebtn' onclick='closeNav()'>×</a><div id='content'><div id='sideheader'><h4 id='headtext'>MENU </h4></div><input type='text' id='mySearch' onkeyup='myFunction()' placeholder='Search..' title='Type in a category'><ul id='myMenu'></ul><div class='debugdiv'><h4>Dark mode</h4><div id='dbgbtn'><label class='switch'><input type='checkbox' id='dbg'><span class='slider' onclick='togdarkmode()'></span></label></div></div></div></div><div id='pagecontent'><div id='heading'><button class='openbtn' id='hamb' onclick='openNav()'>☰</button><div id='title'><h5>"+uname+"'s notebook"+"</h5></div><button class='openbtn' id='graphbtn' onclick='openGraph();setTimeout(showGraph(), 400);'>&#128376;</button></div><div id='main'><h1 id='ContentHeading'>Welcome to Pubsidian<hr></h1><div id='mainContent'><img src='https://raw.githubusercontent.com/yoursamlan/pubsidian/main/CDN/pubsidian.png' alt='logo' style='height:10%; width:auto; display:block; margin-right:auto;float:none !important;'><p>Version: 3.2.0-beta <br>Developed by: <a href='https://github.com/yoursamlan' target='_blank' style='color: red'>Amlan Saha Kundu</a></p><h3>Help</h3><ul><li>Click ☰ on top left to open the MENU.</li><li>Click on your desired note to open.</li><li>To search a note, enter the name of the note in the search-bar</li><li>Click &#128376; on top right to open the Graph.</li><li>On 'Menu', toggle the switch to enable/ disable dark mode.</li></ul></div></div></div><div id='plotgraph'><a href='javascript:void()' id='closegraphbtn' onclick='closegraph(); setTimeout(location.reload.bind(location), 200);'>×</a><div id='GraphView'></div></div></div><img src='https://leancoding.co/DOYB8A' style='width:1px !important;'><script src='https://d3js.org/d3.v3.min.js'></script><script src='https://rawcdn.githack.com/yoursamlan/pubsidian/a7da9546987b84dfc80c52178f802e212d0a6ea8/CDN/network.js' type='text/javascript'></script><script src='https://rawcdn.githack.com/yoursamlan/pubsidian/a7da9546987b84dfc80c52178f802e212d0a6ea8/CDN/script.js' type='text/javascript'></script><script src='https://rawcdn.githack.com/yoursamlan/pubsidian/a7da9546987b84dfc80c52178f802e212d0a6ea8/CDN/data.js' type='text/javascript'></script><script src='https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'></script></body></html>").encode('utf-8'))
+        f.close()
+        
+def UpImgur(PATH):
+    im = pyimgur.Imgur(CLIENT_ID)
+    uploaded_image = im.upload_image(PATH, title=PATH)
+    #print(uploaded_image.title)
+    #print(uploaded_image.size)
+    #print(uploaded_image.type)
+    print("Successfully uploaded to: ",uploaded_image.link)
+    return(uploaded_image.link)
 
 def strip(text,sep):
     return text.split(sep, 1)[0]
@@ -66,14 +82,16 @@ def highlight(text):
     return t
 
    
-
 def md2html(fname):
     inp = fname+'.md'
     outp = "./OUTPUT/pages/"+fname+'.html'
     with open(inp, 'r') as f:
         text = f.read()
-        text = markdown(text, extensions=[WikiLinkExtension(base_url='./', end_url='.html')])
+        #text = markdown(text, extensions=['markdown.extensions.tables'])
+        text = markdown(text, extensions=[WikiLinkExtension(base_url='./', end_url='.html'),'markdown.extensions.tables'])
         text = strip(text,"See more")
+        text = text.replace("![[","![](")
+        text = text.replace("]]",")")
         html = markdown(text)
         html = highlight(html)
         soup = BeautifulSoup(html,"html.parser")
@@ -83,12 +101,15 @@ def md2html(fname):
             hr.extract()
         for img in soup.findAll('img'):
             urlInfo = urlparse(img['src'])
-            img['src'] = "./OUTPUT/pages/Image/" + urlInfo.path
+            impath = base_attachment + urlInfo.path
+            print("Ready to upload ", impath)
+            imgurl = UpImgur(impath)
+            img['src'] = imgurl
         html = str(soup)
-
+        
     with open(outp, 'w') as f:
-        f.write(html) 
-
+        f.write(html)
+        
 def convert(uname):
     directory = r'./'
     if not os.path.isdir(directory+"./OUTPUT"):
@@ -102,7 +123,8 @@ def convert(uname):
                 md2html(filename[:-3])
                 findex.append(filename[:-3])
                 print("Converting -> "+filename)
-            except:
+            except Exception as e:
+                print(e)
                 continue
         else:
             continue
@@ -115,7 +137,7 @@ def convert(uname):
         print("Nothing to convert in "+str(os.path.abspath(os.getcwd()))+". Please make sure the directory has .md files.")
 
 def Welcome():
-    print("\nWelcome to convert2web v1.0.5-beta\n"+"="*40)
+    print("\nWelcome to convert2web v1.2-beta\n"+"="*40)
     uname = input("Enter your name: ")
     convert(uname)
     
